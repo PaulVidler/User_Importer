47321381922b4fc7166809a8b579e4485f99a1a8
Paul Vidler <45616638+PaulVidler@users.noreply.github.com>
2021-11-02 10:20:53 +1000

    #console-1 - CSV User Importer
    Insert working and validating emails. Will not allow duplicate emails. Now unfortunately, I'm out of time.

--- user_upload.php
+++ user_upload.php
@@ -61,19 +61,21 @@ $app->register('import')
         $createTable = $input->getArgument('create_table');
         $dryRun = $input->getArgument('dry_run');
 
-        echo $servername. "\n";
-        echo $username. "\n";
-        echo $password. "\n";
-        echo $filePath. "\n";
-        echo $createTable. "\n";
-        echo gettype($createTable). "\n";
-        echo "dryRun\n";
-        echo $dryRun . "\n";
-        echo gettype($dryRun). "\n";
-
         if ($createTable != null){
             if(isset($servername, $username, $password)){
                 createTable($servername, $username, $password);
+            }
+        }
+
+        if (!$dryRun != false){
+            if(isset($servername, $username, $password)){
+                createTable($servername, $username, $password);
+                try {
+                    $result = InsertUsers($output, $filePath, $servername, $username, $password);
+                    echo $result;
+                } catch (Exception $e) {
+                    echo "There has been an error. Exception caught: " . $e->getMessage() . "\n";
+                }
             }
         }
 
@@ -87,7 +89,6 @@ $app->run();
 
 function createTable($servername, $username, $password){
     
-    echo "From createtable\n";
     $createTable = 'CREATE TABLE IF NOT EXISTS users (
         id INT AUTO_INCREMENT PRIMARY KEY,
         name varchar(255) NOT NULL,
@@ -102,47 +103,14 @@ function createTable($servername, $username, $password){
 
 function showDryRun(OutputInterface $output, $filePath){
     $data = [];
+    $data = ReadCSVFile($output, $filePath);
 
-    // open the file
-    $f = fopen($filePath, 'r');
-
-    if ($f === false) {
-        die('Cannot open the file, check your path is correct ' . $filePath);
-    }
-
-    $count = 1;
-    // read each line in CSV file at a time
-    while (($row = fgetcsv($f)) !== false) {
-        // skip header
-        if ($count === 1){
-            $count++;
-            continue;
-        } 
-        else {
-            // $data[] = $row;
-            // $count++;
-            if(CheckEmail($row[2]) == '' ){
-                $newArr = [CleanSpecialNameStringChars($row[0]), CleanSpecialNameStringChars($row[1]), "** Email did not pass validation **"];
-            } else{
-                $newArr = [CleanSpecialNameStringChars($row[0]), CleanSpecialNameStringChars($row[1]), CheckEmail($row[2])];
-            }
-            
-            array_push($data, $newArr);
-        }
-
-    }
-
-    // close the file
-    fclose($f);
-
-    $table = new Table($output);
-
-    $table->setHeaders(['Name', 'Surname', 'Email'])
-            ->setRows($data)
-            ->render();
+    ShowTable($output, $data);
+    
 }
 
-function InsertToDB(OutputInterface $output, $filePath){
+function ReadCSVFile(OutputInterface $output, $filePath)
+{
     $data = [];
 
     // open the file
@@ -161,10 +129,9 @@ function InsertToDB(OutputInterface $output, $filePath){
             continue;
         } 
         else {
-            // $data[] = $row;
-            // $count++;
+            $count++;
             if(CheckEmail($row[2]) == '' ){
-                $newArr = [CleanSpecialNameStringChars($row[0]), CleanSpecialNameStringChars($row[1]), "** Email did not pass validation **"];
+                $newArr = [CleanSpecialNameStringChars($row[0]), CleanSpecialNameStringChars($row[1]), "** This email does not pass validation **"];
             } else{
                 $newArr = [CleanSpecialNameStringChars($row[0]), CleanSpecialNameStringChars($row[1]), CheckEmail($row[2])];
             }
@@ -177,9 +144,16 @@ function InsertToDB(OutputInterface $output, $filePath){
     // close the file
     fclose($f);
 
-    
+    return $data;
 }
 
+function ShowTable(OutputInterface $output, $data){
+    $table = new Table($output);
+
+    $table->setHeaders(['Name', 'Surname', 'Email'])
+            ->setRows($data)
+            ->render();
+}
 
 // clean up odd chars and whitespace from Name strings - Requires feedback on code review for edge cases
 function CleanSpecialNameStringChars($str)
@@ -188,17 +162,39 @@ function CleanSpecialNameStringChars($str)
     return trim(ucfirst(strtolower($res)));
 }
 
-// Check email - Requires feedback on code review for edge cases
+// Check email - Requires feedback on code review for edge cases, currently does not allow "open.edu.au" domain
 function CheckEmail($email){
     return filter_var(strtolower($email), FILTER_VALIDATE_EMAIL);
 }
 
-function InsertUsers(){
+function InsertUsers(OutputInterface $output, $filePath, $servername, $username, $password){
 
-    // try/catch
     // check email is not duplicated
+    $data = [];
+    $data = ReadCSVFile($output, $filePath);
 
-    // return "success";
+    $pdo = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+
+    $test = $pdo->query("SELECT email FROM users")->fetchAll();
+
+    if(is_array($data)){
+        $count = 0;
+        foreach ($data as $row) {
+
+            if($row[2] == "** This email does not pass validation **"){
+                // echo $row[2]. "\n";
+                echo "User: " .$row[0]. ' '. $row[1]. " email did not pass validation or is already in the DB. Please check this email and try again\n";
+            } else {
+                $date = date('Y-m-d H:i:s');
+                $sql = "INSERT INTO users (name, surname, email, created) VALUES (?,?,?,?)";
+                $stmt= $pdo->prepare($sql);
+                $stmt->execute([$row[0], $row[1], $row[2], $date]);
+                $count++;
+            }
+        }
+    }
+    echo "Results are shown below. All records with an unvalidated email \nhave been skipped and are not in the database\n";
+    showDryRun($output, $filePath);
 }
 
 
